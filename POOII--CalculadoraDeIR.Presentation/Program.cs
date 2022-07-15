9d4941552b6cfc1f95c96956d3eb2da74c1752a0
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-07-14 22:58:19 -0300

    fix: Deletado o projeto POOII-CalculadoraDeIR.Presentation devido ele ser um projeto .net Framework e as bibliotecas de classe da solução serem .net 6.0. Criado  POOII--CalculadoraDeIR.Presentation feat: criado o container na class Program

---
+++ POOII--CalculadoraDeIR.Presentation/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using POOII_CalculadoraDeIR.Service;
+using POOII_CalculadoraDeIR.Service.Interfaces;
+
+namespace POOII_CalculadoraDeIR.Presentation
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            IServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            var serviceProvicer = services.BuildServiceProvider();
+
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<ITaxCalculator, TaxCalculator>();
+
+        }
+    }
+}
+
